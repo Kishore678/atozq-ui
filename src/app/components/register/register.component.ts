a7edc686d0131d022461eb54edbc3a2eef919dd7
S R Kishore Ganta <srkishore.ganta@gmail.com>
2022-10-28 01:56:21 +0530

    design added login register

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -28,7 +28,7 @@ export class RegisterComponent implements OnInit {
             this.router.navigate(["/dashboard"]);          
           },
           err => {
-           
+            console.log(err);
           }
         );;
       },
