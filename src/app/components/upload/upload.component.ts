973250891015591ed120cf9a2a99a3ab93f21959
S R Kishore Ganta <srkishore.ganta@gmail.com>
2023-07-29 23:59:03 +0530

    rating improvements

--- src/app/components/upload/upload.component.ts
+++ src/app/components/upload/upload.component.ts
@@ -30,9 +30,10 @@ const apiBaseUrl = environment.apiBaseUrl;
   styleUrls: ['./upload.component.css'],
   animations: [
     trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('collapsed, void', style({height: '0px'})),
       state('expanded', style({height: '*'})),
       transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+      transition('expanded <=> void', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)'))
     ]),
   ],
 })
@@ -64,8 +65,7 @@ export class UploadComponent {
 displayedColumns = [    
     'Flg',
     'Nme',
-    'LTP',
-    'Cpg',
+    'LTP',  
     'Actions' 
     // 'Code',
     // 'Cnt',
