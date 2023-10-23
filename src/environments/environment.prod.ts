a0459345d2c64d8b74d581ab014d6115f784d023
S R Kishore Ganta <srkishore.ganta@gmail.com>
2023-10-23 20:29:11 +0530

    changed to bsite prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,  
   apiBaseUrl:'https://bsitenet.somee.com',  
-  onlineUsersApi:'https://www.atozq.somee.com'
+  onlineUsersApi:'https://atozq.bsite.net'
 };
