d5baf7cfa085e341ac516e4c54a7ce82dca4e00f
Kishore Ganta <Kishore Ganta@DESKTOP-A6VVRRV>
2024-12-11 16:45:49 +0530

    bpmt publish

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,  
   apiBaseUrl:'https://bsitenet.somee.com',  
-  onlineUsersApi:'https://atozq.somee.com',
+  onlineUsersApi:'https://bpmt.somee.com',
+  // onlineUsersApi:'https://atozq.somee.com',
   // onlineUsersApi:'https://vbraog.somee.com', 
   lbApi:'https://ramki8678.bsite.net'
 };
